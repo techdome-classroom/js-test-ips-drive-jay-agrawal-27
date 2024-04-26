367676b25ac2a571dc9df0caaa1aae16ed15512f
jay-agrawal-27 <167911209+jay-agrawal-27@users.noreply.github.com>
2024-04-26 07:01:33 +0000

    Auto commit on save

--- program2.js
+++ program2.js
@@ -4,7 +4,9 @@ function longestSubstring(s) {
     // Implementation of longestSubstring function
 
     let longestSubstring =0;
-    for(let i=0;i<s.length;)
+    for(let i=0;i<s.length;i++){
+        let currentstring
+    }
 }
 
 module.exports = { longestSubstring };
